21ad3e25857d84c4f4d5467987d4da7c36c5631e
Mila <malaksumaaa@gmail.com>
2025-11-17 20:58:00 +0100

    create react

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -6,8 +7,10 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
