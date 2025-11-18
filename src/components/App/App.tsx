f23a63c4a556c3671bc291eab270d1a535710a73
Mila <malaksumaaa@gmail.com>
2025-11-18 21:44:47 +0100

    create app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import css from './App.module.css';
 import ReactPaginate from 'react-paginate';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
 
-import type { Movie, MoviesServiceResponse } from '../../types/movie';
+import type { Movie } from '../../types/movie';
 import fetchMovies from '../../services/movieService';
 import SearchBar from '../SearchBar/SearchBar';
 import { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import MovieModal from '../MovieModal/MovieModal';
+import type { MoviesServiceResponse } from '../../services/movieService';
 
 function App() {
   const [topic, setTopic] = useState<string>('');
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const [page, setPage] = useState(1);
+
   const { data, isLoading, isError, isSuccess } =
     useQuery<MoviesServiceResponse>({
       queryKey: ['movies', topic, page],
@@ -24,6 +27,15 @@ function App() {
       enabled: topic !== '',
       placeholderData: keepPreviousData,
     });
+  useEffect(() => {
+    if (!isSuccess) {
+      return;
+    }
+    if (!data) return;
+    if (data.results.length === 0) {
+      toast.error('No movies found for your request');
+    }
+  }, [isSuccess, data]);
 
   const totalPages = data?.total_pages ?? 0;
 
@@ -42,6 +54,7 @@ function App() {
         <ReactPaginate
           pageCount={totalPages}
           pageRangeDisplayed={5}
+          initialPage={page}
           marginPagesDisplayed={1}
           onPageChange={({ selected }) => setPage(selected + 1)}
           forcePage={page - 1}
